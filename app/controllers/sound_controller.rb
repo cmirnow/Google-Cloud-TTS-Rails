b8e69c7897b6841f2861af27f174d4a0e7a82ce1
rishard <info@masterpro.ws>
2020-01-14 17:54:16 +0300

    maintenance

--- app/controllers/sound_controller.rb
+++ app/controllers/sound_controller.rb
@@ -1,24 +1,25 @@
 class SoundController < ApplicationController
-	before_action :authenticate_admin!
-			def index
-		unless params[:request].nil? 
-						request = Validation.new(form_params)
-			if request.valid?
-				client = Google::Cloud::TextToSpeech.new
-				synthesis_input = { text: params[:request] }
-				voice = {
-					language_code: params[:lang],
-					name: params[:voicename]
-				}
-				audio_config = { audio_encoding: "MP3" }
-				TtsConversion.index(client, synthesis_input, voice, audio_config)
-				flash[:success] = '<a href="/output/output.mp3" download>Download mp3</a>'.html_safe
-			else
-				flash[:error] = 'Invalid request format'
-			end
-		end
-	end
-    def form_params
-		params.permit(:request)
-	end
+  before_action :authenticate_admin!
+  def index
+    unless params[:request].nil?
+      request = Validation.new(form_params)
+      if request.valid?
+        client = Google::Cloud::TextToSpeech.new
+        synthesis_input = { text: params[:request] }
+        voice = {
+          language_code: params[:lang],
+          name: params[:voicename]
+        }
+        audio_config = { audio_encoding: 'MP3' }
+        TtsConversion.index(client, synthesis_input, voice, audio_config)
+        flash[:success] = '<a href="/output/output.mp3" download>Download mp3</a>'.html_safe
+      else
+        flash[:error] = 'Invalid request format'
+      end
+    end
+  end
+
+  def form_params
+    params.permit(:request)
+   end
 end
--- app/services/tts_conversion.rb
+++ app/services/tts_conversion.rb
@@ -1,8 +1,8 @@
 class TtsConversion
-	def self.index(client, synthesis_input, voice, audio_config)
-		response = client.synthesize_speech synthesis_input, voice, audio_config
-		File.open "public/output/output.mp3", "wb" do |file|
-			file.write response.audio_content
-		end
+  def self.index(client, synthesis_input, voice, audio_config)
+    response = client.synthesize_speech synthesis_input, voice, audio_config
+    File.open 'public/output/output.mp3', 'wb' do |file|
+      file.write response.audio_content
     end
+  end
 end
