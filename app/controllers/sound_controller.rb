0a70909b5aefafa5f35fba752a6aca9717e3b7a4
rishard <info@masterpro.ws>
2020-03-05 00:51:09 +0300

    code improvement

--- app/controllers/sound_controller.rb
+++ app/controllers/sound_controller.rb
@@ -1,27 +1,52 @@
 class SoundController < ApplicationController
   before_action :authenticate_admin!
+
   def index
     unless params[:request].nil?
-      request = Validation.new(form_params)
-      if request.valid?
-        client = Google::Cloud::TextToSpeech.new
-        synthesis_input = { params[:text_or_ssml] => params[:request] }
-        voice = {
-          language_code: params[:lang],
-          name: params[:voicename]
-        }
-        audio_config = { audio_encoding: params[:codec] }
-        audio_format = TtsConversion.index(client, synthesis_input, voice, audio_config, params[:codec])
-        flash[:success] = "<a href='/output/output.#{audio_format}' download>Download #{audio_format}</a>".html_safe
+      if check.valid?
+        conversion
       else
-        flash[:error] = 'Invalid request format'
+        error_info
       end
     end
   end
 
-private
+  def conversion
+    audio_format = TtsConversion.index(client, synthesis_input, voice, audio_config, params[:codec])
+    success_info(audio_format)
+  end
+
+  def client
+    Google::Cloud::TextToSpeech.new
+  end
+
+  def synthesis_input
+    { params[:text_or_ssml] => params[:request] }
+  end
+
+  def voice
+    { language_code: params[:lang], name: params[:voicename] }
+  end
+
+  def audio_config
+    { audio_encoding: params[:codec] }
+  end
+
+  def check
+    Validation.new(form_params)
+  end
+
+  def success_info(audio_format)
+    flash[:success] = "<a href='/output/output.#{audio_format}' download>Download #{audio_format}</a>".html_safe
+  end
+
+  def error_info
+    flash[:error] = 'Invalid request format'
+  end
+
+  private
+
   def form_params
     params.permit(:request)
-   end
+  end
 end
-
