f055b6731054ae117cfdf9c747566bcb2ffb14f3
rishard <info@masterpro.ws>
2020-08-22 18:13:21 +0300

    added new languages

--- public/js/chained.js
+++ public/js/chained.js
@@ -84,9 +84,81 @@ function generateOptions() {
 				'en-US-Standard-E': null
 			}
 		},
+		'fr-FR': {
+			'WaveNet': {
+				'fr-FR-WaveNet-A': null,
+				'fr-FR-WaveNet-B': null,
+				'fr-FR-WaveNet-C': null,
+				'fr-FR-WaveNet-D': null,
+				'fr-FR-WaveNet-E': null
+			},
+			'Basic': {
+				'fr-FR-Standard-E': null,
+				'fr-FR-Standard-A': null,
+				'fr-FR-Standard-B': null,
+				'fr-FR-Standard-C': null,
+				'fr-FR-Standard-D': null
+			}
+		},
+		'de-DE': {
+			'WaveNet': {
+				'de-DE-WaveNet-F': null,
+				'de-DE-WaveNet-A': null,
+				'de-DE-WaveNet-B': null,
+				'de-DE-WaveNet-C': null,
+				'de-DE-WaveNet-D': null,
+				'de-DE-WaveNet-E': null
+			},
+			'Basic': {
+				'de-DE-Standard-E': null,
+				'de-DE-Standard-A': null,
+				'de-DE-Standard-B': null,
+				'de-DE-Standard-F': null
+			}
+		},
 		'es-ES': {
 			'Basic': {
 				'es-ES-Standard-A': null
+			}
+		},
+		'fi-FI': {
+			'WaveNet': {
+				'fi-FI-WaveNet-A': null
+			},
+			'Basic': {
+				'fi-FI-Standard-A': null
+			}
+		},
+		'nl-NL': {
+			'WaveNet': {
+				'nl-NL-WaveNet-B': null,
+				'nl-NL-WaveNet-C': null,
+				'nl-NL-WaveNet-D': null,
+				'nl-NL-WaveNet-E': null,
+				'nl-NL-WaveNet-A': null
+			},
+			'Basic': {
+				'nl-NL-Standard-A': null,
+				'nl-NL-Standard-B': null,
+				'nl-NL-Standard-C': null,
+				'nl-NL-Standard-D': null,
+				'nl-NL-Standard-E': null
+			}
+		},
+		'pl-PL': {
+			'WaveNet': {
+				'pl-PL-WaveNet-A': null,
+				'pl-PL-WaveNet-B': null,
+				'pl-PL-WaveNet-C': null,
+				'pl-PL-WaveNet-D': null,
+				'pl-PL-WaveNet-E': null
+			},
+			'Basic': {
+				'pl-PL-Standard-E': null,
+				'pl-PL-Standard-A': null,
+				'pl-PL-Standard-B': null,
+				'pl-PL-Standard-C': null,
+				'pl-PL-Standard-D': null
 			}
 		},
 		'ru-RU': {
