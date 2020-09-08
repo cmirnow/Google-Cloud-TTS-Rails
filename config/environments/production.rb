c7f30527fc3168e0421577f0899b932cb8bec7e3
rishard <info@masterpro.ws>
2020-09-08 23:45:40 +0300

    environment variables add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://YOUR_HEROKU_DOMAIN.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
