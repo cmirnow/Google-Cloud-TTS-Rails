3adee1f1a7c5725ab51ee31c2066830b6516cab9
rishard <info@masterpro.ws>
2021-10-09 09:58:02 +0300

    code improvement

--- app/services/tts_conversion.rb
+++ app/services/tts_conversion.rb
@@ -3,11 +3,18 @@ class TtsConversion
     response = args[0].synthesize_speech input: args[1],
                                          voice: args[2],
                                          audio_config: args[3]
-    File.open 'public/output/output.' +
-              audio_format(args[4]).to_s,
+    file_write(args[4], response)
+  end
+
+  def self.file_write(codec, response)
+    File.open Rails.root.join(
+      'public',
+      'output',
+      "#{'output'}.#{audio_format(codec)}"
+    ),
               'wb' do |file|
       file.write response.audio_content
-      audio_format(args[4]).to_s
+      audio_format(codec)
     end
   end
 
